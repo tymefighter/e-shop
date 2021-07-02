982aa645aec7f2a0078cb39ba5ada506b0121595
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-02 17:33:11 +0530

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Nav from "./Nav";
 
 function App() {
-  return null;
+  return (
+    <Nav />
+  );
 }
 
 export default App;
