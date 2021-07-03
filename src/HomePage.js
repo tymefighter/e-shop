475a35f7f4e369b1dfe4c9c4d4c0503e55b6b126
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-03 20:23:57 +0530

    Add CSS

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,23 +1,25 @@
 import React from "react";
 
+import "./styles/HomePage.scss";
+
 const HomePage = function() {
     return (
-        <>
-            <h1>Welcome to E-Shop</h1>
-            <p>The <strong>Best</strong> Platform for Buyers and Sellers alike</p>
-            <p>Empowering Buyers and Enhancing Sellers is our motto</p>
-            <p>Immerse into a whole new kind of market experience</p>
-            <p>We rely on Advanced AI techniques to cater the needs of our users</p>
-            <p>
+        <div className="home">
+            <h1 className="home__heading">Welcome to E-Shop</h1>
+            <p className="home__para">The <strong>Best</strong> Platform for Buyers and Sellers alike</p>
+            <p className="home__para">Empowering Buyers and Enhancing Sellers is our motto</p>
+            <p className="home__para">Immerse into a whole new kind of market experience</p>
+            <p className="home__para">We rely on Advanced AI techniques to cater the needs of our users</p>
+            <p className="home__para">
                 We are working on building the first VR based marketplace, where{" "}
                 the sellers can provide a virtual model of their product, and{" "}
                 buyers can virtually experience that product.
             </p>
-            <p>We lead the market in providing the users with immensely scalable platform</p>
-            <p>
+            <p className="home__para">We lead the market in providing the users with immensely scalable platform</p>
+            <p className="home__para">
                 We welcome you once again, to the future of marketplace.
             </p>
-        </>
+        </div>
     );
 };
 
