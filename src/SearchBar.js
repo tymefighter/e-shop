08e233f22ead31fa3e27a7835fa33dbadd43fb8d
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-07-03 11:03:24 +0530

    Add sort order

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 import "./styles/SearchBar.scss";
 
-const SearchBar = function({searchQuery, setSearchQuery}) {
+const SearchBar = function({
+    searchQuery, setSearchQuery, 
+    priceOrder, setPriceOrder
+}) {
     return (
         <div className="search-bar">
             <span className="fa fa-4x fa-search search-bar__icon"></span>
@@ -12,6 +15,15 @@ const SearchBar = function({searchQuery, setSearchQuery}) {
                 onChange={(event) => setSearchQuery(event.target.value)}
                 value={searchQuery}
             />
+            <select 
+                className="search-bar__order-input"
+                name="priceOrder" 
+                onChange={(event) => setPriceOrder(event.target.value)}
+                value={priceOrder}
+            >
+                <option value="increase"> Increasing </option>
+                <option value="decrease"> Decreasing </option>
+            </select>
         </div>
     );
 };
--- src/UserPage.js
+++ src/UserPage.js
@@ -4,15 +4,28 @@ import ProductGrid from "./ProductGrid";
 
 const UserPage = function({productList}) {
     const [searchQuery, setSearchQuery] = useState("");
+    const [priceOrder, setPriceOrder] = useState("");
+
+    const filteredProductList = productList.filter(({name, description}) => {
+        return name.toLowerCase().includes(searchQuery)
+            || description.toLowerCase().includes(searchQuery);
+    });
+
+    filteredProductList.sort((prodA, prodB) => {
+        if(prodA.price === prodB.price) return 0;
+
+        if(priceOrder === "increase") return prodA.price < prodB.price ? -1 : 1;
+        else return prodA.price < prodB.price ? 1 : -1;
+    });
 
     return (
         <div>
-            <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+            <SearchBar 
+                searchQuery={searchQuery} setSearchQuery={setSearchQuery} 
+                priceOrder={priceOrder} setPriceOrder={setPriceOrder}
+            />
             <ProductGrid 
-                productList={productList.filter(({name, description}) => {
-                    return name.toLowerCase().includes(searchQuery)
-                        || description.toLowerCase().includes(searchQuery);
-                })} 
+                productList={filteredProductList} 
             />
         </div>
     );
